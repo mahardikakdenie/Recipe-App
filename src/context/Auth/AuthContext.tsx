6b923dbbae26a85c9cb242b90279a51f5fbbbf83
mahardikakdenie <kucingemas744@gmail.com>
2025-11-21 20:30:13 +0700

    chore(authcontext): authcontent api setup

---
+++ src/context/Auth/AuthContext.tsx
@@ -0,0 +1,42 @@
+// /src/contexts/Auth/AuthContext.tsx
+import React, { createContext, useContext, useEffect, useState } from 'react'; 
+
+interface User {
+    email: string;
+    password: string;
+}
+
+type AuthContextType = {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = async (email: string, password: string) => {
+    // ... logic login
+  };
+
+  const logout = () => setUser(null);
+
+  useEffect(() => {
+    console.log("Haloo");
+    
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within AuthProvider');
+  return context;
+};
